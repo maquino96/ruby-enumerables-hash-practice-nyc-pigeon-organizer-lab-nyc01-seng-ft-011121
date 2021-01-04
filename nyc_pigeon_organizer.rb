c2a0f6ff5f01a661ecd6380a904b18c5e2202e72
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 17:13:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  result_hash = {}
+  pigeon_list = [] 
+  data[:gender][:male].each { |el| pigeon_list.push(el) }
+  data[:gender][:female].each { |el| pigeon_list.push(el)}
+  
+  #puts data[:gender][:male]
+  #puts pigeon_list
+  
+  pigeon_list.each do |el|
+    result_hash[el] = {
+    :color => [],
+    :gender => [],
+    :lives => []
+  }
+  end 
+
+  #puts result_hash  
+
+  data.each do |o_key, o_value|
+    data[o_key].each do |i_key, i_value|
+      data[o_key][i_key].each { |ele| result_hash[ele][o_key].push(i_key.to_s)}
+    end
+  end
+
+result_hash
 end
+
+

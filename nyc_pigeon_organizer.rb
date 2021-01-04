40df91d4499ca1196f48c122d942d3a46ec3064b
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 17:12:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,3 @@
-
-#pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-#}
-
 def nyc_pigeon_organizer(data)
   
   result_hash = {}
@@ -47,35 +27,4 @@ def nyc_pigeon_organizer(data)
 result_hash
 end
 
-#nyc_pigeon_organizer(pigeon_data)
-
-#sorted_data = { 
-  "Alex"=>{ :color=>["white", "brown"], 
-            :gender=>["male"], 
-            :lives=>["Central Park"]},
-            
-  "Theo"=>{ :color=>["purple", "grey"], 
-            :gender=>["male"], 
-            :lives=>["Subway"]}, 
-            
-  "Peter Jr."=>{  :color=>["purple", "grey"], 
-                  :gender=>["male"], 
-                  :lives=>["Library"]}, 
-                  
-  "Andrew"=>{ :color=>["white"], 
-              :gender=>["male"], 
-              :lives=>["City Hall"]},
-              
-  "Lucky"=>{  :color=>["purple"], 
-              :gender=>["male"], 
-              :lives=>["Central Park"]}, 
-              
-  "Queenie"=>{  :color=>["white", "brown"], 
-                :gender=>["female"], 
-                :lives=>["Subway"]}, 
-                
-  "Ms. K"=>{  :color=>["grey", "white"], 
-              :gender=>["female"], 
-              :lives=>["Central Park"]}
-#}
 
